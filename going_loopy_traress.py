6f480dcd0d417ee13ddd54769cbfaedfe272bc7b
Sam <traress28@cbchs.org>
2024-09-18 19:28:09 -0500

    going loopy

---
+++ going_loopy_traress.py
@@ -0,0 +1,22 @@
+age = -2
+
+for total_age in range (-2, 6):
+    age = age + 2 
+    print(age)
+
+
+toppings = ['lettuce', 'tomato', 'bacon', 'ham', 'turkey']
+
+number = 0
+
+for topping in toppings:
+    number = number + 1
+    print(f'\nTopping {number}: {topping}')
+print('Done!\n')
+
+
+weight = 21.45
+
+for Year in range (1, 16):
+    weight = weight + 2.205
+    print('Year %s = %s' % (Year, weight))
